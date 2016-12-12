851466774ff745da71a98d4ff6cf1062378eb43e
7WMcNulty <7WMcNulty@stpatrick.org>
2016-12-12 13:28:07 -0600

    Item Class

---
+++ item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor(n,p,s){
+    this.name = n;
+    this.price = p;
+    this.shipping = s;
+  }
+
+
+}
+
+//Create your three test items below this comment.
+var book = new Item("Robin Hood", 9.99, 1);
+var movie = new Item("Impractical Jokers", 19.99, 2);
+var clothes = new Item("shirt", 14.99, 3);
