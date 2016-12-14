f62d3722c678896a22f09914c462795ca976df8d
7WMcNulty <7WMcNulty@stpatrick.org>
2016-12-14 14:07:36 -0600

    Still working on valid private key

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,21 +1,3 @@
-class Item{
-  //Fill in the item class below this comment.
-  constructor(n,p,s){
-    this.name = n;
-    this.price = p;
-    this.shipping = s;
-  }
-
-
-}
-
-//Create your three test items below this comment.
-var book = new Item("Robin Hood", 9.99, 1);
-var movie = new Item("Impractical Jokers", 19.99, 2);
-var clothes = new Item("shirt", 14.99, 3);
-let l1 = [book];
-let l2 = [1];
-
 class Cart{
   //What is the first part of every class? Type it below.
   constructor(l,q){
@@ -48,6 +30,3 @@ class Cart{
   }
 
 }
-var test = new Cart(l1,l2);
-test.addItem(movie,2);
-console.log(test.totalCart());
--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,11 +1,55 @@
 class Password{
   //The first part of the class.
-
+  constructor(pub,priv){
+    this.publickey = pub;
+    this.privatekey = priv;
+  }
 
 
   //Instance functions below this comment.
+  validPublicKey(){
+    if(this.publickey.length >=8 && this.publickey.length <= 25){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  validPrivateKey(){
+    if(this.privatekey.charAt(4) == "-" && this.privatekey.charAt(9) == "-"){
+      for(var i=0;i<this.privatekey.length;i++){
+        let test = Number(temp.charAt(i));
 
 
+
+        if(Number.isNaN(test)){
+          console.log("bad");
+        }
+        else if(Number.isNaN(test)){
+          console.log("good");
+        }
+        }
+      }
+    }
+  }
 
   //Static function below this comment.
+  static makePrivateKey(){
+    var key = "";
+    var limit = 14;
+    for(let i=0; i<limit; i++){
+      if(i==4 || i==9){
+        key = key+"-";
+      }
+      else{
+        let temp = Math.random();
+        temp = temp*10;
+        temp = Math.floor(temp);
+        key = key+temp;
+      }
+    }
+    return key;
+  }
 }
+var test = new Password("1234567812345678123456789","8888-8888-8888");
+console.log(Password.makePrivateKey());
