970bd2e8b8c48ebb5437f1fffc8fe7ee01475afb
7WMcNulty <7WMcNulty@stpatrick.org>
2016-12-16 15:10:55 -0600

    This is the final version

---
+++ Final Code/password.js
@@ -0,0 +1,54 @@
+class Password{
+  //The first part of the class.
+  constructor(pub,priv){
+    this.publickey = pub;
+    this.privatekey = priv;
+  }
+
+
+  //Instance functions below this comment.
+  validPublicKey(){
+    if(this.publickey.length >=8 && this.publickey.length <= 25){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  validPrivateKey(){
+    if(this.privatekey[4] !== "-"||this.privatekey[9] !== "-"){
+      return false;
+    }
+    else if(Number.isNaN(Number(this.privatekey.substring(0,4)))){
+      return false;
+    }
+    else if(Number.isNaN(Number(this.privatekey.substring(5,9)))){
+      return false;
+    }
+    else if(Number.isNaN(Number(this.privatekey.substring(10,14)))){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+  //Static function below this comment.
+  static makePrivateKey(){
+    var key = "";
+    var limit = 14;
+    for(let i=0; i<limit; i++){
+      if(i==4 || i==9){
+        key = key+"-";
+      }
+      else{
+        let temp = Math.random();
+        temp = temp*10;
+        temp = Math.floor(temp);
+        key = key+temp;
+      }
+    }
+    return key;
+  }
+}
+var test = new Password("1234567812345678123456789","8888-8888-8888");
+console.log(test.validPrivateKey());
